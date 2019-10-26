2adbdd3d99b430c5236a9fe946b36f54e2d5fb35
Mahmoud Elshakoushy <mahmoudelshakoushy@mahmouds-mac.local>
2019-10-26 02:09:02 -0700

    Initial Commit

---
+++ SafeNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafeNote
+//
+//  Created by Mahmoud Elshakoushy on 10/26/19.
+//  Copyright © 2019 Mahmoud Elshakoushy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
